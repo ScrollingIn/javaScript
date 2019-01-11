3036765133af6c95c5e0e1e241d15176dd6ce697
ScrollingIn <mdejonge@roc-dev.com>
2019-01-11 11:40:32 +0100

    Finsished assignment 5.1, busy/taking a break with/from 5.2

---
+++ javaScript5/kittenMemory.js
@@ -0,0 +1,79 @@
+let A = document.createElement(img);
+A.src = 'kitten1.jpg';
+let B = document.createElement(img);
+B.src = 'kitten2.jpg';
+let C = document.createElement(img);
+C.src = 'kitten3.jpg';
+let D = document.createElement(img);
+D.src = 'kitten4.jpg';
+let E = document.createElement(img);
+E.src = 'kitten5.jpg';
+let F = document.createElement(img);
+F.src = 'kitten6.jpg';
+let G = document.createElement(img);
+G.src = 'kitten7.jpg';
+let H = document.createElement(img);
+H.src = 'kitten8.jpg';
+let I = document.createElement(img);
+I.src = 'kitten9.jpg';
+var memory_array = [A, A, B, B, C, C, D, D, E, E, F, F, G, G, H, H, I, I];
+var memory_values = [];
+var memory_tile_ids = [];
+var tiles_flipped = 0;
+Array.prototype.memory_tile_shuffle = function(){
+    var i = this.length, j, temp;
+    while(--i > 0){
+        j = Math.floor(Math.random() * (i+1));
+        temp = this[j];
+        this[j] = this[i];
+        this[i] = temp;
+    }
+}
+function newBoard(){
+    tiles_flipped = 0;
+    var output = '';
+    memory_array.memory_tile_shuffle();
+    for(var i = 0; i < memory_array.length; i++){
+        output += '<div id="tile_'+i+'" onclick="memoryFlipTile(this,\''+memory_array[i]+'\')"></div>';
+    }
+    document.getElementById('memory_board').innerHTML = output;
+}
+function memoryFlipTile(tile,val){
+    if(tile.innerHTML == "" && memory_values.length < 2){
+        tile.style.background = '#FFF';
+        tile.innerHTML = val;
+        if(memory_values.length == 0){
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+        } else if(memory_values.length == 1){
+            memory_values.push(val);
+            memory_tile_ids.push(tile.id);
+            if(memory_values[0] == memory_values[1]){
+                tiles_flipped += 2;
+                // Clear both arrays
+                memory_values = [];
+                memory_tile_ids = [];
+                // Check to see if the whole board is cleared
+                if(tiles_flipped == memory_array.length){
+                    alert("Board cleared... generating new board");
+                    document.getElementById('memory_board').innerHTML = "";
+                    newBoard();
+                }
+            } else {
+                function flip2Back(){
+                    // Flip the 2 tiles back over
+                    var tile_1 = document.getElementById(memory_tile_ids[0]);
+                    var tile_2 = document.getElementById(memory_tile_ids[1]);
+                    tile_1.style.background = 'url(JS_Logo.png) no-repeat';
+                    tile_1.innerHTML = "";
+                    tile_2.style.background = 'url(JS_Logo.png) no-repeat';
+                    tile_2.innerHTML = "";
+                    // Clear both arrays
+                    memory_values = [];
+                    memory_tile_ids = [];
+                }
+                setTimeout(flip2Back, 700);
+            }
+        }
+    }
+}
